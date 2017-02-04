196cae14743c3ea4399b9ab276f72c02cd4012bb
Hans Keller <kellerhd@gmail.com>
2017-02-04 16:48:16 -0500

    Add files via upload

--- _Botnuki/CommandHandler.cs
+++ _Botnuki/CommandHandler.cs
@@ -29,34 +29,43 @@ namespace _Botnuki
 
                 await commands.AddModuleAsync<RolesModule>().ConfigureAwait(false);
                 Console.WriteLine("Roles Module Installed!");
-
+                //await commands.AddModuleAsync<BanModule>().ConfigureAwait(false);
                 client.MessageReceived += HandleCommand;
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ErrorHandling.ThrowGenException("CommandHandler.cs", "Install(IDependencyMap)", ex.Message));
+                return; 
             }
         }
 
         public static async Task HandleCommand(SocketMessage parameterMessage)
         {
-            // Don't handle the command if it is a system message
-            var message = parameterMessage as SocketUserMessage;
-            if (message == null) return;
+            try
+            {
+                // Don't handle the command if it is a system message
+                var message = parameterMessage as SocketUserMessage;
+                if (message == null) return;
 
-            // Mark where the prefix ends and the command begins
-            int argPos = 0;
-            // Determine if the message has a valid prefix, adjust argPos 
-            if (!(message.HasMentionPrefix(client.CurrentUser, ref argPos) || message.HasCharPrefix('/', ref argPos))) return;
+                // Mark where the prefix ends and the command begins
+                int argPos = 0;
+                // Determine if the message has a valid prefix, adjust argPos 
+                if (!(message.HasMentionPrefix(client.CurrentUser, ref argPos) || message.HasCharPrefix('/', ref argPos))) return;
 
-            // Create a Command Context
-            var context = new CommandContext(client, message);
-            // Execute the Command, store the result
-            var result = await commands.ExecuteAsync(context, argPos, map);
+                // Create a Command Context
+                var context = new CommandContext(client, message);
+                // Execute the Command, store the result
+                var result = await commands.ExecuteAsync(context, argPos, map);
 
-            // If the command failed, notify the user
-            if (!result.IsSuccess)
-                await message.Channel.SendMessageAsync($"**Error:** {result.ErrorReason}");
+                // If the command failed, notify the user
+                if (!result.IsSuccess)
+                    await message.Channel.SendMessageAsync($"**Error:** {result.ErrorReason}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ErrorHandling.ThrowGenException("CommandHandler.cs", "HandleCommand(SocketMessage)", ex.Message));
+                return;
+            }
         }
     }
 }
